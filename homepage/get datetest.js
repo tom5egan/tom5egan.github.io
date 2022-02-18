2be1dc5ee68ef40a837b5f87820384a5770ee8c0
tom5egan <tom5egan@gmail.com>
2022-02-18 03:00:14 -0600

    remove unused pictures

--- homepage/get datetest.js
+++
@@ -1,9 +0,0 @@
- //gets the system time from the machine running the webpage
- var currentMonth = new Date().getMonth ( )+1;
- var currentDay = new Date().getDate ( );
- var currentYear = new Date().getFullYear ( );
- 
-   // Compose the string for display
- var currentDateString = currentMonth + "/" + currentDay + "/" + currentYear;
-
- console.log(currentDateString);
